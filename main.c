6c57f3157eeb6e3e6d7ab1dfd7b247719659b03b
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-05-24 11:54:25 -0700

    update1

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
  */
 int main(int argc, char **argv)
 {
+	(void)argc;
+	(void)argv;
 	prompt();
 
 	return (0);
--- prompt.c
+++ prompt.c
@@ -12,7 +12,7 @@ void prompt(void)
 		n_char = getline(&command, &size_command, stdin);
 		if (n_char == EOF)
 			perror("getline problem");
-		command[n_char - 1] = "\0";
+		command[n_char - 1] = '\0';
 		if (strcmp(command, "exit") == 0)
 		{
 			break;
